711652e331a4f99b9fac27b2f2aa5a4ee0c0937c
alansarluv <alan.sarluf@gmail.com>
2019-12-03 19:07:36 +0700

    scraping multiple page

--- index.js
+++ index.js
@@ -13,15 +13,13 @@ const scrapingResults = [
   }
 ]
 
-async function main() {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+async function scrapeListing(page) {
   await page.goto(
     "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/pen/sof"
   );
   const html = await page.content();
   const $ = cheerio.load(html);
-  const results = $(".result-info")
+  const listings = $(".result-info")
     .map((index, element) => {
       const titleElement = $(element).find(".result-title");
       const timeElement = $(element).find(".result-date");
@@ -36,8 +34,33 @@ async function main() {
         .replace(")", "")
       return { title, url, datePosted, hood };
     }).get();
-console.log(results);
+  return listings;
 };
+
+async function scrapeJobDescriptions(listings, page) {
+  for (let i = 0; i<listings.length; i++) {
+    await page.goto(listings[i].url);
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const jobDescription = $("#postingbody").text();
+    listings[i].jobDescription = jobDescription;
+    await sleep(1000); // 1 second sleep
+
+    console.log(listings[i].jobDescription);
+  }
+}
+
+async function sleep(miliseconds) {
+  return new Promise(resolve => setTimeout(resolve, miliseconds));
+}
+
+async function main() {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  const listings = await scrapeListing(page);
+  const listingsWithJobDescriptions = await scrapeJobDescriptions(listings, page);
+  // console.log(listings);
+}
 
 
 main();
